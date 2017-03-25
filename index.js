cd5ac363d66430aa8e9c15ff29df1e52629e2bbc
Julian Gruber <julian@juliangruber.com>
2017-03-25 10:27:03 +0100

    refactor using native eventemitter, bump coverage to 100%

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 var assert = require('assert')
+var EventEmitter = require('events').EventEmitter
 
 module.exports = Nanobus
 
 function Nanobus () {
   if (!(this instanceof Nanobus)) return new Nanobus()
-  this._starListeners = []
-  this._starOnces = []
-  this._listeners = {}
-  this._onces = {}
+  this._emitter = new EventEmitter()
 }
 
 Nanobus.prototype.emit = function (eventName, data) {
   assert.equal(typeof eventName, 'string', 'nanobus.emit: eventName should be type string')
-
-  if (this._starListeners.length) {
-    this._emit(this._starListeners, eventName, data)
-  }
-  if (this._starOnces.length) {
-    this._emit(this._starOnces, eventName, data)
-    this._starOnces = []
-  }
-
-  var listeners = this._listeners[eventName]
-  if (listeners && listeners.length) this._emit(listeners, data)
-
-  var onces = this._onces[eventName]
-  if (onces && onces.length) {
-    this._emit(onces, data)
-    this._onces[eventName] = []
+  this._emitter.emit(eventName, data)
+  var listeners = this.listeners('*')
+  for (var i = 0; i < listeners.length; i++) {
+    listeners[i].call(this._emitter, data)
   }
   return this
 }
@@ -35,93 +21,32 @@ Nanobus.prototype.emit = function (eventName, data) {
 Nanobus.prototype.on = Nanobus.prototype.addListener = function (eventName, listener) {
   assert.equal(typeof eventName, 'string', 'nanobus.on: eventName should be type string')
   assert.equal(typeof listener, 'function', 'nanobus.on: listener should be type function')
-
-  if (eventName === '*') {
-    this._starListeners.push(listener)
-  } else {
-    if (!this._listeners[eventName]) this._listeners[eventName] = []
-    this._listeners[eventName].push(listener)
-  }
+  this._emitter.on(eventName, listener)
   return this
 }
 
 Nanobus.prototype.once = function (eventName, listener) {
   assert.equal(typeof eventName, 'string', 'nanobus.once: eventName should be type string')
   assert.equal(typeof listener, 'function', 'nanobus.once: listener should be type function')
-
-  if (eventName === '*') {
-    this._starOnces.push(listener)
-  } else {
-    if (!this._onces[eventName]) this._onces[eventName] = []
-    this._onces[eventName].push(listener)
-  }
+  this._emitter.once(eventName, listener)
   return this
 }
 
 Nanobus.prototype.removeListener = function (eventName, listener) {
   assert.equal(typeof eventName, 'string', 'nanobus.removeListener: eventName should be type string')
   assert.equal(typeof listener, 'function', 'nanobus.removeListener: listener should be type function')
-
-  if (eventName === '*') {
-    if (remove(this._starOnces, listener)) return this
-    if (remove(this._starListeners, listener)) return this
-  } else {
-    if (remove(this._listeners[eventName], listener)) return this
-    if (remove(this._onces[eventName], listener)) return this
-  }
-
-  function remove (arr, listener) {
-    if (!arr) return
-    var index = arr.indexOf(listener)
-    if (index !== -1) {
-      arr.splice(index, 1)
-      return true
-    }
-  }
+  this._emitter.removeListener(eventName, listener)
 }
 
 Nanobus.prototype.removeAllListeners = function (eventName) {
   if (eventName) {
-    if (eventName === '*') {
-      this._starListeners = []
-      this._starOnces = []
-    } else {
-      this._listeners[eventName] = []
-      this._onces[eventName] = []
-    }
+    this._emitter.removeAllListeners(eventName)
   } else {
-    this._starListeners = []
-    this._starOnces = []
-    this._listeners = {}
-    this._onces = {}
+    this._emitter.removeAllListeners()
   }
   return this
 }
 
 Nanobus.prototype.listeners = function (eventName) {
-  var listeners = this._listeners[eventName]
-  var onces = this._onces[eventName]
-  var ret = []
-  if (listeners) {
-    var ilength = listeners.length
-    for (var i = 0; i < ilength; i++) ret.push(listeners[i])
-  }
-  if (onces) {
-    var jlength = onces.length
-    for (var j = 0; j < jlength; j++) ret.push(onces[j])
-  }
-  return ret
-}
-
-Nanobus.prototype._emit = function (arr, eventName, data) {
-  if (!data) {
-    data = eventName
-    eventName = null
-  }
-  var length = arr.length
-  for (var i = 0; i < length; i++) {
-    var listener = arr[i]
-    if (eventName) listener(eventName, data)
-    else listener(data)
-  }
+  return this._emitter.listeners(eventName)
 }
